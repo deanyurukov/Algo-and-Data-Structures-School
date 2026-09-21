7ad8c0e29616b79d0da0b5736dbb1082673ca765
Dean Yurukov <dean.yurukov2008@gmail.com>
2026-09-21 12:39:31 +0300

    01/EgyptionFraction

---
+++ 01.GreedyAlgorithms/MoneyGet/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace MoneyGet;
+
+public class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> values = new List<int>() { 50, 20, 10, 5, 2, 1 };
+        int numToGet = int.Parse(Console.ReadLine());
+
+        int initial = numToGet;
+
+        while (initial > 0)
+        {
+            foreach (int number in values)
+            {
+                if (initial >= number)
+                {
+                    Console.WriteLine(number);
+                    initial -= number;
+                    break;
+                }
+            }
+        }
+    }
+}
